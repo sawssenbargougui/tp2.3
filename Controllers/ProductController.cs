7cf3763b8e9def9e1ab19ee36cfb749b1e64e82e
Your Name <you@example.com>
2024-10-12 12:24:22 +0100

    first

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,146 @@
+﻿using ATELIER2.Models;
+using ATELIER2.Models.Repositories;
+using ATELIER2.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATELIER2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Injection de dépendance
+        readonly IRepository<Product> ProductRepository;
+        private readonly IWebHostEnvironment hostingEnvironment;
+
+        public ProductController(IRepository<Product> ProdRepository, IWebHostEnvironment hostingEnvironment)
+        {
+            ProductRepository = ProdRepository;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        public ActionResult Index()
+        {
+            var Products= ProductRepository.GetAll();
+            return View(Products);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = null;
+                // If the Photo property on the incoming model object is not null, then the user has selected an image to upload.
+                if (model.ImagePath != null)
+                {
+                    // The image must be uploaded to the images folder in wwwroot
+                    // To get the path of the wwwroot folder we are using the inject
+                    // HostingEnvironment service provided by ASP.NET Core
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    // To make sure the file name is unique we are appending a new
+                    // GUID value and an underscore to the file name
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImagePath.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    // Use CopyTo() method provided by IFormFile interface to
+                    // copy the file to wwwroot/images folder
+                    model.ImagePath.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+                Product newProduct = new Product
+
+                {
+                    Désignation = model.Désignation,
+                    Prix = model.Prix,
+                    Quantite = model.Quantite,
+                    // Store the file name in PhotoPath property of the employee object
+                    // which gets saved to the Employees database table
+                    Image = uniqueFileName
+                };
+                ProductRepository.Add(newProduct);
+                return RedirectToAction("Details", new { id = newProduct.Id });
+            }
+            return View();
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Product product = ProductRepository.Get(id);
+            EditViewModel productEditViewModel = new EditViewModel
+            {
+                Id = product.Id,
+                Désignation = product.Désignation,
+                Prix = product.Prix,
+                Quantite = product.Quantite,
+                ExistingImagePath = product.Image
+            };
+
+            return View(productEditViewModel);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = ProductRepository.Get(model.Id);
+                product.Désignation = model.Désignation;
+                product.Prix = model.Prix;
+                product.Quantite = model.Quantite;
+
+                if (model.ImagePath != null)
+                {
+                    if (model.ExistingImagePath != null)
+                    {
+                        string filePath = Path.Combine(hostingEnvironment.WebRootPath, "images", model.ExistingImagePath);
+                        System.IO.File.Delete(filePath);
+                    }
+
+                    product.Image = ProcessUploadedFile(model);
+                }
+
+                Product updatedProduct = ProductRepository.Update(product);
+                if (updatedProduct != null)
+                    return RedirectToAction("Index");
+                else
+                    return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [NonAction]
+        
+        private string ProcessUploadedFile(dynamic model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ImagePath != null)
+            {
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImagePath.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ImagePath.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
+        }
+
+    }
+}
---
+++ Models/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ATELIER2.Models.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Get(int Id);
+        IEnumerable<T> GetAll();
+        T Add(T t);
+        T Update(T t);
+        T Delete(int Id);
+    }
+}
